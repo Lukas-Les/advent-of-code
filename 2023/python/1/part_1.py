151037242b35108d2bbfb09f2764ed0fc3b52c4b
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2023-12-01 18:30:48 +0200

    2023 1 part_1 python update

---
+++ 2023/python/1/part_1.py
@@ -0,0 +1,20 @@
+def load_input():
+    with open("input.txt", "r") as f:
+        return f.read().strip()
+
+
+def parse_line(_line):
+    for char in _line:
+        if char.isnumeric():
+            return int(char)
+    return ""
+
+
+result = 0
+
+for line in load_input().split("\n"):
+    number = f"{parse_line(line)}{parse_line(line[::-1])}"
+    if number:
+        result += int(number)
+
+print(result)
