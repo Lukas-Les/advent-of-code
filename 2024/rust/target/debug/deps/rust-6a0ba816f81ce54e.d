539e22d1829ed3469bd91a0d8499ee2a9faa1657
stabmeldys <lukas.les93@gmail.com>
2024-12-01 20:10:58 +0200

    deleter /target

--- 2024/rust/target/debug/deps/rust-6a0ba816f81ce54e.d
+++
@@ -1,9 +0,0 @@
-/mnt/e/github/advent-of-code/2024/rust/target/debug/deps/librust-6a0ba816f81ce54e.rmeta: src/lib.rs src/day1/mod.rs src/day1/part1.rs
-
-/mnt/e/github/advent-of-code/2024/rust/target/debug/deps/librust-6a0ba816f81ce54e.rlib: src/lib.rs src/day1/mod.rs src/day1/part1.rs
-
-/mnt/e/github/advent-of-code/2024/rust/target/debug/deps/rust-6a0ba816f81ce54e.d: src/lib.rs src/day1/mod.rs src/day1/part1.rs
-
-src/lib.rs:
-src/day1/mod.rs:
-src/day1/part1.rs:
--- 2024/rust/target/debug/deps/rust-b29279874931aae8.d
+++
@@ -1,7 +0,0 @@
-/mnt/e/github/advent-of-code/2024/rust/target/debug/deps/librust-b29279874931aae8.rmeta: src/lib.rs src/day1/mod.rs src/day1/part1.rs
-
-/mnt/e/github/advent-of-code/2024/rust/target/debug/deps/rust-b29279874931aae8.d: src/lib.rs src/day1/mod.rs src/day1/part1.rs
-
-src/lib.rs:
-src/day1/mod.rs:
-src/day1/part1.rs:
