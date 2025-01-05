35e0219dee5769ee079db3ea33ae57fbb657dc85
stabmeldys <lukas.les93@gmail.com>
2025-01-05 20:01:47 +0200

    still fails

--- 2024/rust/src/day2/part1.rs
+++ 2024/rust/src/day2/part1.rs
@@ -1,28 +1,38 @@
 use crate::day2::load_reports;
 
-fn is_breaking(i: u32, j: u32) -> bool {
-    (i as i32 - j as i32).abs() > 2
+
+const LIMIT: u32 = 4;
+
+
+fn is_breaking(i: u32, j: u32, increasing: bool) -> bool {
+    if i == j {
+        return true;
+    }
+    if increasing {
+        i < j && (j - i > LIMIT)
+    } else {
+        j < i && (i - j > LIMIT)
+    }
 }
 
 
 fn solve() -> u32 {
     let reports = load_reports("input1.txt");
-    //reports
-    //    .into_iter()
-    //    .map(|report| {
-    //        report
-    //            .windows(2)
-    //            .filter(|pair| !is_breaking(pair[0], pair[1]))
-    //            .count() as u32
-    //    })
-    //    .sum()
     let mut result = 0;
     for report in reports.iter() {
         let mut safe = true;
+        let mut is_increasing: Option<bool> = None;
         for i in 0..report.len() - 1 {
-            if is_breaking(report[i], report[i + 1]) {
+            if is_increasing.is_none() {
+                if report[i] < report[i + 1] {
+                    is_increasing = Some(true);
+                } else {
+                    is_increasing = Some(false);
+                }
+            }
+            if is_breaking(report[i], report[i + 1], is_increasing.unwrap()) {
                 safe = false;
-                break
+                break;
             }
         }
         if safe {
